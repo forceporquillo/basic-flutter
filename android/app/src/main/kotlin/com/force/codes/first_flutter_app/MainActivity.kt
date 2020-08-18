7d4b195ea8a49a926e47c9867b05be26093affed
Force Porquillo <fporquillo18@gmail.com>
2020-08-18 23:52:43 +0800

    flutter practice

---
+++ android/app/src/main/kotlin/com/force/codes/first_flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.force.codes.first_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Center(
+        child: GradientButton(),
+      ),
+    ),
+  ));
+}
+
+class GradientButton extends StatefulWidget {
+  @override
+  _GradientButtonState createState() => _GradientButtonState();
+}
+
+class _GradientButtonState extends State<GradientButton>
+    with SingleTickerProviderStateMixin {
+  double _scale = 35.0;
+
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+      lowerBound: 0.0,
+      upperBound: 0.2,
+    )..addListener(() {
+        setState(() {
+          _showToast(title: "SpaceX");
+        });
+      });
+  }
+
+  void _showToast({String title}) {
+    Fluttertoast.showToast(
+      msg: title,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.CENTER,
+      backgroundColor: Color(0xFFe74c3c),
+      textColor: Color(0xFFFFFFFF)
+    );
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _controller.reverse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _scale = 1 - _controller.value;
+
+    return GestureDetector(
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      child: Transform.scale(
+        scale: _scale,
+        child: _buttonUI(),
+      ),
+    );
+  }
+
+  Widget _buttonUI() {
+    return Container(
+      width: 300.0,
+      height: 80.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(1000),
+        boxShadow: [
+          BoxShadow(
+              color: Color(0xFFFF512F).withOpacity(0.35),
+              offset: Offset(0.0, 22.0),
+              blurRadius: 44.0,
+              spreadRadius: -12.0),
+        ],
+        gradient: LinearGradient(
+          colors: const [Color(0xFF4B90EB), Color(0xFFC41494)],
+        ),
+      ),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              "Henlo fren!",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Icon(Icons.arrow_forward_ios, color: Colors.white),
+          ],
+        ),
+      ),
+    );
+  }
+}
